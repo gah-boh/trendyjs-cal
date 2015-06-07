b160d83cf0a097ecee2e85379ab45ac277027474
Gabriel Obregon <gabo.obregon@gmail.com>
2015-06-06 22:55:27 -0400

    Chore: Formatting fixes with new eslint rules.

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 // Karma configuration
 // Generated on Sat Apr 25 2015 13:42:36 GMT-0400 (EDT)
 
@@ -9,71 +11,71 @@ delete webpackTestConfig.entry;
 delete webpackTestConfig.output;
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'app/',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: 'app/',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      '**/*_test.js'
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-      'node_modules'
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            '**/*_test.js'
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      '**/*_test.js': ['webpack', 'sourcemap']
-    },
-
-    webpack: webpackTestConfig,
-
-    webpackMiddleware: {
-        noInfo: true
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'notify'],
+        // list of files to exclude
+        exclude: [
+            'node_modules'
+        ],
 
 
-    // web server port
-    port: 9876,
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            '**/*_test.js': ['webpack', 'sourcemap']
+        },
+
+        webpack: webpackTestConfig,
+
+        webpackMiddleware: {
+            noInfo: true
+        },
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'notify'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  });
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    });
 };
--- serve.js
+++ serve.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 var express = require('express');
 var path = require('path');
 
